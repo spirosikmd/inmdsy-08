89dde5e1b9bbc420a57627c5b6f8604d965af848
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-26 01:14:55 +0200

    ignore sublime project files

--- src/multicast/Multicast.java
+++ src/multicast/Multicast.java
@@ -1,101 +1,7 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
-public class Multicast implements Runnable, ReadEventListener {
-
-	protected InetAddress group;
-	protected int port;
-
-	public Multicast(InetAddress group, int port) {
-		this.group = group;
-		this.port = port;
-		initIO();
-	}
-
-	protected BufferedReader input;
-	protected PrintStream output;
-
-	protected void initIO() {
-		input = new BufferedReader(new InputStreamReader(System.in));
-		output = System.out;
-	}
-
-	protected Thread listener;
-	protected Thread readSourceListener;
-
-	public synchronized void start() throws IOException {
-		if (listener == null) {
-			initNet();
-			listener = new Thread(this);
-			listener.start();
-			ReadSource readSource = new ReadSource(input, output);
-			readSourceListener = new Thread(readSource);
-			readSourceListener.start();
-			readSource.addEventListener(this);
-		}
-	}
-
-	protected MulticastSocket socket;
-	protected DatagramPacket outgoing, incoming;
-
-	protected void initNet() throws IOException {
-		socket = new MulticastSocket(port);
-		socket.setTimeToLive(5);
-		socket.joinGroup(group);
-		outgoing = new DatagramPacket (new byte[1], 1, group, port);
-		incoming = new DatagramPacket (new byte[65508], 65508);
-	}
-
-	public synchronized void stop () throws IOException {
-		if (listener != null) {
-			listener.interrupt();
-			listener = null;
-			try {
-				socket.leaveGroup(group);
-			} finally {
-				socket.close();
-			}
-		}
-	}
-
-	public void handleReadEvent(ReadEvent e) {
-		try {
-			byte[] utf = e.getReadInput();
-			outgoing.setData(utf);
-			outgoing.setLength(utf.length);
-			socket.send(outgoing);
-		} catch (IOException ex) {
-			handleIOException(ex);
-		}
-	}
-
-	public void run() {
-		try {
-			while (!Thread.interrupted()) {
-				incoming.setLength(incoming.getData().length);
-				socket.receive(incoming);
-				String message = new String(
-					incoming.getData(), 0, incoming.getLength(), "UTF8");
-				output.println(message);
-			}
-		} catch (IOException ex) {
-			handleIOException(ex);
-		}
-	}
-
-	protected synchronized void handleIOException (IOException ex) {
-		if (listener != null) {
-			output.println(ex);
-			if (listener != Thread.currentThread())
-				listener.interrupt();
-			listener = null;
-			try {
-				socket.leaveGroup(group);
-			} catch (IOException ignored) {}
-			socket.close();
-		}
-	}
+public class Multicast {
 
 	public static void main(String[] args) throws IOException {
 		if ((args.length != 1) || (args[0].indexOf (":") < 0))
@@ -106,7 +12,7 @@ public class Multicast implements Runnable, ReadEventListener {
 		InetAddress group = InetAddress.getByName(args[0].substring(0, idx));
 		int port = Integer.parseInt(args[0].substring(idx + 1));
 
-		Multicast multicast = new Multicast(group, port);
-		multicast.start();
+		Peer peer = new Peer(group, port);
+		peer.start();
 	}
 }
---
+++ src/multicast/Peer.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Peer implements Runnable, ReadEventListener {
+
+	protected InetAddress group;
+	protected int port;
+
+	public Peer(InetAddress group, int port) {
+		this.group = group;
+		this.port = port;
+		initIO();
+	}
+
+	protected BufferedReader input;
+	protected PrintStream output;
+
+	protected void initIO() {
+		input = new BufferedReader(new InputStreamReader(System.in));
+		output = System.out;
+	}
+
+	protected Thread listener;
+	protected Thread readSourceListener;
+
+	public synchronized void start() throws IOException {
+		if (listener == null) {
+			initNet();
+			listener = new Thread(this);
+			listener.start();
+			ReadSource readSource = new ReadSource(input, output);
+			readSourceListener = new Thread(readSource);
+			readSourceListener.start();
+			readSource.addEventListener(this);
+		}
+	}
+
+	protected MulticastSocket socket;
+	protected DatagramPacket outgoing, incoming;
+
+	protected void initNet() throws IOException {
+		socket = new MulticastSocket(port);
+		socket.setTimeToLive(5);
+		socket.joinGroup(group);
+		outgoing = new DatagramPacket (new byte[1], 1, group, port);
+		incoming = new DatagramPacket (new byte[65508], 65508);
+	}
+
+	public synchronized void stop () throws IOException {
+		if (listener != null) {
+			listener.interrupt();
+			listener = null;
+			try {
+				socket.leaveGroup(group);
+			} finally {
+				socket.close();
+			}
+		}
+	}
+
+	public void handleReadEvent(ReadEvent e) {
+		try {
+			byte[] utf = e.getReadInput();
+			outgoing.setData(utf);
+			outgoing.setLength(utf.length);
+			socket.send(outgoing);
+		} catch (IOException ex) {
+			handleIOException(ex);
+		}
+	}
+
+	public void run() {
+		try {
+			while (!Thread.interrupted()) {
+				incoming.setLength(incoming.getData().length);
+				socket.receive(incoming);
+				String message = new String(
+					incoming.getData(), 0, incoming.getLength(), "UTF8");
+				output.println(message);
+			}
+		} catch (IOException ex) {
+			handleIOException(ex);
+		}
+	}
+
+	protected synchronized void handleIOException (IOException ex) {
+		if (listener != null) {
+			output.println(ex);
+			if (listener != Thread.currentThread())
+				listener.interrupt();
+			listener = null;
+			try {
+				socket.leaveGroup(group);
+			} catch (IOException ignored) {}
+			socket.close();
+		}
+	}
+}
