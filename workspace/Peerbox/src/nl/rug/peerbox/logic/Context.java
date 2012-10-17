9bc1c9e32ce4c0380f73332edf8f86bd7013205b
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-10-17 15:35:25 +0200

    strange

--- workspace/Peerbox/src/nl/rug/peerbox/logic/Context.java
+++ workspace/Peerbox/src/nl/rug/peerbox/logic/Context.java
@@ -1,6 +1,6 @@
 package nl.rug.peerbox.logic;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 import nl.rug.peerbox.middleware.Multicast;
 
@@ -14,10 +14,10 @@ public interface Context {
 
 	public int getPort();
 	
-<<<<<<< HEAD:workspace/FindingPeers/src/nl/rug/peerbox/logic/Context.java
-	public ArrayList<FileDescriptor> getVirtualFilesystem(); // Map<Host, String[]>
-=======
+
+	//public ArrayList<FileDescriptor> getVirtualFilesystem(); // Map<Host, String[]>
+
 	public Map<Peer, String[]> getVirtualFilesystem();
->>>>>>> c39c4675943e89f08b1d029a6d45eab7463cbb03:workspace/Peerbox/src/nl/rug/peerbox/logic/Context.java
+
 	
 }
