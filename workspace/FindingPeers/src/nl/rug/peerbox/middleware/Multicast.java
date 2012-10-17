08e502cb354467848fe481917de1a8a9d84c873b
Christian Manteuffel <cm@notagain.de>
2012-10-17 14:23:01 +0200

    multicast class

---
+++ workspace/FindingPeers/src/nl/rug/peerbox/middleware/Multicast.java
@@ -0,0 +1,12 @@
+package nl.rug.peerbox.middleware;
+
+
+public interface Multicast {
+
+	
+	public void announce(byte[] message);
+	
+	public void addMessageListener(MessageListener ml);
+	
+	public void shutdown();	
+}
