966b50577c6dd8bd5be8333348d714e8550d9db3
Christian Manteuffel <cm@notagain.de>
2012-10-17 11:31:48 +0200

    multi threaded server

--- workspace/FindingPeers/src/nl/rug/peerbox/logic/FileServer.java
+++ workspace/FindingPeers/src/nl/rug/peerbox/logic/FileServer.java
@@ -9,46 +9,67 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 
 final class FileServer implements Runnable {
-	
-	private final String path;
-	private final int port;
+
+	private final class SendFileTask implements Runnable {
+		private final Socket s;
+		private final Context ctx;
+
+		private SendFileTask(Socket s, Context ctx) {
+			this.s = s;
+			this.ctx = ctx;
+		}
+
+		@Override
+		public void run() {
+			try {
+				BufferedReader st = new BufferedReader(
+						new InputStreamReader(s
+								.getInputStream()));
+				String fileid = st.readLine();
+				logger.info("File " + fileid
+						+ " has been requested.");
+				File myFile = new File(ctx.getPathToPeerbox() + "/" + fileid);
+				byte[] mybytearray = new byte[(int) myFile
+						.length()];
+
+				BufferedInputStream bis = new BufferedInputStream(
+						new FileInputStream(myFile));
+
+				bis.read(mybytearray, 0, mybytearray.length);
+
+				OutputStream os = s.getOutputStream();
+				os.write(mybytearray, 0, mybytearray.length);
+				os.flush();
+				bis.close();
+			} catch (IOException e) {
+				logger.error(e);
+			}
+		}
+	}
+
+	private final Context ctx;
 
 	private static final Logger logger = Logger.getLogger(FileServer.class);
-	
+	private final ExecutorService pool = Executors.newCachedThreadPool();
+
 	FileServer(Context ctx) {
-		this.path = ctx.getPathToPeerbox();
-		this.port = ctx.getPort();
+		this.ctx = ctx;
 	}
 
 	@Override
 	public void run() {
-		try (ServerSocket server = new ServerSocket(port)) {
+		try (ServerSocket server = new ServerSocket(ctx.getPort())) {
 
-			while (true) {
-				try (Socket s = server.accept()) {
-
-					BufferedReader st = new BufferedReader(
-							new InputStreamReader(s.getInputStream()));
-					String fileid = st.readLine();
-					System.out.println("The requested file is : "
-							+ path + "/" + fileid);
-					File myFile = new File(path + "/" + fileid);
-					byte[] mybytearray = new byte[(int) myFile.length()];
-
-					BufferedInputStream bis = new BufferedInputStream(
-							new FileInputStream(myFile));
-
-					bis.read(mybytearray, 0, mybytearray.length);
-
-					OutputStream os = s.getOutputStream();
-					os.write(mybytearray, 0, mybytearray.length);
-					os.flush();
-					bis.close();
-
+			while (Thread.currentThread().isInterrupted()) {
+				try {
+					final Socket s = server.accept();
+					pool.execute(new SendFileTask(s, ctx));
 				} catch (IOException e) {
 					logger.error(e);
 				}
